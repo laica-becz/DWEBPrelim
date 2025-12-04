195f6f510694a3dde820a755e518877e8629942b
laica-becz <zepedabiancaellise@gmail.com>
2025-12-04 12:58:37 +0800

    Add files via upload

---
+++ includes/footer.php
@@ -0,0 +1,3 @@
+    <footer>Created by: Bianca Ellise C. Zepeda &copy; <?php echo date('Y')?></footer>
+  </body>
+</html>
---
+++ includes/header.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>BBJ Bread & Pastries</title>
+    <link rel="stylesheet" href="css/styles.css">
+  </head>
+  <body>
+    <h1>BBJ Bread & Pastries</h1>
+    <nav>
+      <a href="index.php">Home</a> | 
+      <a href="koreanbread.php">Bread & Pastries</a> | 
+      <a href="about.php">About</a> | 
+      <a href="contact.php">Contact</a>
+    </nav>
